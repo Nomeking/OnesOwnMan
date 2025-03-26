d6047d3333128df388a95370d1e7de8d1e3b5783
Nomeking <odyrns99@hotmail.com>
2025-03-26 23:53:43 +0200

    Currently can't build the project although it rebuilds fine

--- Source/OnesOwnMan/PlayerCharacter.h
+++ Source/OnesOwnMan/PlayerCharacter.h
@@ -1,9 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "PaperZDCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "PlayerCharacter.generated.h"
 
 /**
@@ -14,4 +14,14 @@ class ONESOWNMAN_API APlayerCharacter : public APaperZDCharacter
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UCameraComponent* Camera;
+
+	APlayerCharacter();
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
 };
